0e3de8d182877a5d34df342f1d0fe62aac1e0a2f
seancabahug <25161923+seancabahug@users.noreply.github.com>
2020-11-16 18:28:28 -0700

    Begin implementing Chrome extension contribution method

--- bot.js
+++ bot.js
@@ -10,6 +10,12 @@ const APIUtil = require('./utils/APIUtil');
 
 var schedule = require('node-schedule');
 
+const ConfirmationFor = {
+    COOKIE_FILE: 0,
+    EXTENSION: 1,
+    CHOOSING_METHOD: 2
+};
+
 client.on('ready', () => {
     console.log("ready")
     const channel = client.guilds.fetch("617617235795640330").then(guild => {return guild.channels.cache.find(channel => channel.id == "617830904634802176")})
@@ -51,8 +57,22 @@ client.on('message', async message => {
                     }
                 });
                 break;
-            default:
-                await message.channel.send("Invalid command. Please type `.help` for a list of available commands.");
+            case 'contribute':
+                if (!awaitingConfirmation.has(message.author.id)){
+                    if (message.channel.type != "dm") await message.channel.send("Thank you for showing interest in contributing! Please refer to DM for more information.");
+                    var confirmationMsg = await message.author.send({
+                        embed: {
+                            title: "Thank you for the interest in contributing!",
+                            description: "Please react accordingly to the contribution process you'd like to go through.\n\n1️⃣ Temporary Chrome Extension **(recommended)**\n2️⃣ Upload `Cookies` File (works if you've logged into the Teams desktop app before 2020)"
+                        }
+                    });
+                    await confirmationMsg.react("1️⃣");
+                    await confirmationMsg.react("2️⃣");
+                    awaitingConfirmation.set(message.author.id, {confirmationMessage: confirmationMsg.id, for: ConfirmationFor.CHOOSING_METHOD});
+                    break;
+                } else {
+                    await message.channel.send("You still have a message requesting your input, please react to any previous messages in your DM that you haven't given an answer to yet.");
+                }
         }
     } else if (message.attachments.size >= 1) {
         if (message.channel.type == 'dm') {
@@ -75,7 +95,7 @@ client.on('message', async message => {
                                 {
                                     "name": "By reacting with :white_check_mark:...",
                                     "value": "you agree that you are aware of the access you're granting and that you trust the bot with this access. If you disagree, react with :x: and your `Cookies` file will not be stored nor processed."
-                                },                                
+                                },
                             ],
                             "footer": {
                                 "text": "Bot developed by @seancabahug (check out my GitHub!)"
@@ -84,9 +104,9 @@ client.on('message', async message => {
                     });
                     await confirmationMsg.react("✅");
                     await confirmationMsg.react("❌");
-                    awaitingConfirmation.set(message.author.id, {confirmationMessage: confirmationMsg.id, cookiesUrl: message.attachments.first().url})
+                    awaitingConfirmation.set(message.author.id, {confirmationMessage: confirmationMsg.id, cookiesUrl: message.attachments.first().url, for: ConfirmationFor.COOKIE_FILE});
                 } else {
-                    await message.channel.send("There is an unattended `Cookies` confirmation, please scroll up and react to the confirmation titled \"HOLD ON!\" with either :white_check_mark: or :x: depending on your choice before uploading another `Cookies` file.");
+                    await message.channel.send("There is an unattended confirmation, please scroll up and react to the confirmation titled \"HOLD ON!\" with either :white_check_mark: or :x: depending on your choice before uploading another `Cookies` file.");
                 }
             }
         } else {
@@ -105,41 +125,87 @@ client.on('messageReactionAdd', async (messageReaction, user) => {
     if(awaitingConfirmation.has(user.id)) {
         const confirmationInformation = awaitingConfirmation.get(user.id);
         if (messageReaction.message.id == confirmationInformation.confirmationMessage) {
-            if (messageReaction.emoji.name == "✅"){
-                awaitingConfirmation.delete(user.id);
-                await messageReaction.message.channel.send("Confirmed, thank you! Downloading your `Cookies` file for processing...");
-                const cookiesCache = `./cache/${user.id}`;
-                const fileStream = fs.createWriteStream(cookiesCache);
-                https.get(confirmationInformation.cookiesUrl, res => {
-                    res.pipe(fileStream);
-                    fileStream.on('finish', async () => {
-                        fileStream.close();
-                        await messageReaction.message.channel.send(`Downloaded! Processing file...`);
-                        var cookiesDb = new sqlite3.Database(cookiesCache);
-                        cookiesDb.get("SELECT value FROM cookies WHERE name='ESTSAUTHPERSISTENT'", async (err, row) => {
-                            if(!err) {
-                                APIUtil.fetchToken(row.value)
-                                    .then(res => {
-                                        const token = res.request.res.responseUrl.split("&")[0].split("=")[1];
-                                        const tokenPayload = JSON.parse(Buffer.from(token.split(".")[1], "base64").toString("utf8"));
-                                        messageReaction.message.channel.send(`Processing successful, thank you ${tokenPayload.given_name}! It is advisable to delete your \`Cookies\` file from this Discord DM.`);
-                                    })
-                                    .catch(res => {
-                                        messageReaction.message.channel.send("There was an error while processing; if this happens again, you may want to try signing in/out from Teams and reuploading your `Cookies` file again. It is advisable to delete your most recently uploaded `Cookies` file.");
-                                        console.log(res);
+            if (confirmationInformation.for != ConfirmationFor.CHOOSING_METHOD) {
+                if (messageReaction.emoji.name == "✅"){
+                    awaitingConfirmation.delete(user.id);
+                    switch (confirmationInformation.for) {
+                        case ConfirmationFor.COOKIE_FILE:
+                            await messageReaction.message.channel.send("Confirmed, thank you! Downloading your `Cookies` file for processing...");
+                            const cookiesCache = `./cache/${user.id}`;
+                            const fileStream = fs.createWriteStream(cookiesCache);
+                            https.get(confirmationInformation.cookiesUrl, res => {
+                                res.pipe(fileStream);
+                                fileStream.on('finish', async () => {
+                                    fileStream.close();
+                                    await messageReaction.message.channel.send(`Downloaded! Processing file...`);
+                                    var cookiesDb = new sqlite3.Database(cookiesCache);
+                                    cookiesDb.get("SELECT value FROM cookies WHERE name='ESTSAUTHPERSISTENT'", async (err, row) => {
+                                        if(!err) {
+                                            APIUtil.fetchToken(row.value)
+                                                .then(res => {
+                                                    const token = res.request.res.responseUrl.split("&")[0].split("=")[1];
+                                                    const tokenPayload = JSON.parse(Buffer.from(token.split(".")[1], "base64").toString("utf8"));
+                                                    messageReaction.message.channel.send(`Processing successful, thank you ${tokenPayload.given_name}! It is advisable to delete your \`Cookies\` file from this Discord DM.`);
+                                                })
+                                                .catch(res => {
+                                                    messageReaction.message.channel.send("There was an error while processing; if this happens again, you may want to try signing in/out from Teams and reuploading your `Cookies` file again. It is advisable to delete your most recently uploaded `Cookies` file.");
+                                                    console.log(res);
+                                                });
+                                        } else {
+                                            await messageReaction.message.channel.send("There was an error while processing; make sure you're uploading your `Cookies` file from the correct location. The `Cookies` file you uploaded has not been saved, and it is advisable to delete it.");
+                                        }
                                     });
-                            } else {
-                                await messageReaction.message.channel.send("There was an error while processing; make sure you're uploading your `Cookies` file from the correct location. The `Cookies` file you uploaded has not been saved, and it is advisable to delete it.");
+                                    cookiesDb.close(() => {
+                                        fs.unlinkSync(cookiesCache);
+                                    });
+                                });
+                            });
+                            break;
+                        case ConfirmationFor.EXTENSION:
+                            // TO-DO implement
+                            break;
+                    }
+                } else if (messageReaction.emoji.name == "❌"){
+                    await messageReaction.message.channel.send("Cancelled. No sensitive information has been collected. It is advisable to remove any sensitive data (including long encoded text or your `Cookies` file) from this Discord DM.");
+                    awaitingConfirmation.delete(user.id);
+                }
+            } else {
+                switch (messageReaction.emoji.name) {
+                    case "1️⃣":
+                        var confirmationMsg = await messageReaction.message.channel.send({
+                            embed: {
+                                "title": "**HOLD ON!**",
+                                "description": "Before we continue, let's make sure you know what's really happening. You can check out the [bot's source code](https://github.com/seancabahug/UHDiscordBot) if you're skeptical.",
+                                "color": 0xFF0000,
+                                "fields": [
+                                    {
+                                        "name": "The data that the Chrome extension will allow you to copy will contain **sensitive information** that can only be abused by people who know what they're doing.",
+                                        "value": "Though it doesn't store your username or password, it can grant people access to your account. However, it is vital for the bot to use it so that it is able to see assignments in classes it doesn't have access to."
+                                    },
+                                    {
+                                        "name": "WE WILL NOT USE THIS ACCESS WITH MALICIOUS INTENT. You can check the source code.",
+                                        "value": "The bot will ONLY use it for seeing your assignments. It WILL NOT GATHER personal information about you (exception being your name); only the assignments, its due dates, and classes pertaining to it will be stored and used for everyone's benefit. The important parts of the data you will paste with the Chrome extension will also be stored PRIVATELY for future resyncing."
+                                    },
+                                    {
+                                        "name": "By reacting with :white_check_mark:...",
+                                        "value": "you agree that you are aware of the access you're granting and that you trust the bot with this access. If you disagree, react with :x: and this will be cancelled."
+                                    },
+                                ],
+                                "footer": {
+                                    "text": "Bot developed by @seancabahug (check out my GitHub!)"
+                                }
                             }
                         });
-                        cookiesDb.close(() => {
-                            fs.unlinkSync(cookiesCache);
-                        });
-                    });
-                });
-            } else if (messageReaction.emoji.name == "❌"){ 
-                await messageReaction.message.channel.send("Cancelled. No sensitive information has been collected. It is advisable to delete your `Cookies` file from this Discord DM.");
-                awaitingConfirmation.delete(user.id);
+                        await confirmationMsg.react("✅");
+                        await confirmationMsg.react("❌");
+                        awaitingConfirmation.delete(user.id);
+                        awaitingConfirmation.set(messageReaction.message.author.id, {confirmationMessage: confirmationMsg.id, for: ConfirmationFor.COOKIE_FILE});
+                        break;
+                    case "2️⃣":
+                        await messageReaction.message.channel.send("This method should be used as a fallback if the Chrome extension does not work. If you would like to continue, please upload the file named `Cookies` located at `%appdata%/Microsoft/Teams` to this DM.");
+                        break;
+                }
+
             }
         }
     }
